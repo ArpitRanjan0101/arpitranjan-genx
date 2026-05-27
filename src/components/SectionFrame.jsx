0bd2b1df3efab4fc74b0e7e0fe25c0a1b1050b59
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2026-05-28 01:46:17 +0530

    feat: create SectionFrame component with decorative border frames and crosshairs

--- src/components/SectionFrame.jsx
+++ src/components/SectionFrame.jsx
@@ -6,22 +6,22 @@ export default function SectionFrame({ children, className, id }) {
     <section id={id} className={cn("relative", className)}>
       {/* Top Frame Line & Crosshairs */}
       <div className="absolute inset-x-0 top-0 mx-auto w-[calc(100%-1rem)] sm:w-[min(calc(100%-1.5rem),74rem)] pointer-events-none z-0">
-         <div className="absolute top-0 left-[-1.5rem] right-[-1.5rem] h-px bg-white/10" />
-         <div className="absolute top-[-4px] left-0 w-px h-[9px] bg-white/30" />
-         <div className="absolute top-[-4px] right-0 w-px h-[9px] bg-white/30" />
-         <div className="absolute top-[-1px] left-[-4px] w-[9px] h-px bg-white/30" />
-         <div className="absolute top-[-1px] right-[-4px] w-[9px] h-px bg-white/30" />
+         <div className="absolute top-0 left-0 right-0 h-px bg-white/10" />
+         <div className="absolute top-[-2px] left-0 w-px h-[5px] bg-white/80" />
+         <div className="absolute top-[-2px] right-0 w-px h-[5px] bg-white/80" />
+         <div className="absolute top-0 left-[-2px] w-[5px] h-px bg-white/80" />
+         <div className="absolute top-0 right-[-2px] w-[5px] h-px bg-white/80" />
       </div>
 
       {children}
 
       {/* Bottom Frame Line & Crosshairs */}
       <div className="absolute inset-x-0 bottom-0 mx-auto w-[calc(100%-1rem)] sm:w-[min(calc(100%-1.5rem),74rem)] pointer-events-none z-0">
-         <div className="absolute bottom-0 left-[-1.5rem] right-[-1.5rem] h-px bg-white/10" />
-         <div className="absolute bottom-[-4px] left-0 w-px h-[9px] bg-white/30" />
-         <div className="absolute bottom-[-4px] right-0 w-px h-[9px] bg-white/30" />
-         <div className="absolute bottom-[-1px] left-[-4px] w-[9px] h-px bg-white/30" />
-         <div className="absolute bottom-[-1px] right-[-4px] w-[9px] h-px bg-white/30" />
+         <div className="absolute bottom-0 left-0 right-0 h-px bg-white/10" />
+         <div className="absolute bottom-[-2px] left-0 w-px h-[5px] bg-white/80" />
+         <div className="absolute bottom-[-2px] right-0 w-px h-[5px] bg-white/80" />
+         <div className="absolute bottom-0 left-[-2px] w-[5px] h-px bg-white/80" />
+         <div className="absolute bottom-0 right-[-2px] w-[5px] h-px bg-white/80" />
       </div>
     </section>
   )
