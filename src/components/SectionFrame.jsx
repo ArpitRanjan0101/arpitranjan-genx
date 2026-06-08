0846f45c93e2ebdcf72b9b09f8eb82e0fdf8e5d7
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2026-06-09 01:54:25 +0530

    feat: update SectionFrame component with corner brackets and enhance globals.css background visibility

--- src/components/SectionFrame.jsx
+++ src/components/SectionFrame.jsx
@@ -4,24 +4,32 @@ import { cn } from '@/utils/cn'
 export default function SectionFrame({ children, className, id }) {
   return (
     <section id={id} className={cn("relative", className)}>
-      {/* Top Frame Line & Crosshairs */}
+      {/* Top Frame Line & Corner Brackets */}
       <div className="absolute inset-x-0 top-0 mx-auto w-[calc(100%-1rem)] sm:w-[min(calc(100%-1.5rem),74rem)] pointer-events-none z-0">
-         <div className="absolute top-0 left-0 right-0 h-px bg-white/20" />
-         <div className="absolute top-[-2px] left-0 w-px h-[5px] bg-white/80" />
-         <div className="absolute top-[-2px] right-0 w-px h-[5px] bg-white/80" />
-         <div className="absolute top-0 left-[-2px] w-[5px] h-px bg-white/80" />
-         <div className="absolute top-0 right-[-2px] w-[5px] h-px bg-white/80" />
+        <div className="absolute top-0 left-0 right-0 h-px bg-white/35" />
+
+        {/* Top-left corner bracket */}
+        <div className="absolute top-0 left-0 w-[1.5px] h-[16px] bg-white/60" />
+        <div className="absolute top-0 left-0 w-[16px] h-[1.5px] bg-white/60" />
+
+        {/* Top-right corner bracket */}
+        <div className="absolute top-0 right-0 w-[1.5px] h-[16px] bg-white/60" />
+        <div className="absolute top-0 right-0 w-[16px] h-[1.5px] bg-white/60" />
       </div>
 
       {children}
 
-      {/* Bottom Frame Line & Crosshairs */}
+      {/* Bottom Frame Line & Corner Brackets */}
       <div className="absolute inset-x-0 bottom-0 mx-auto w-[calc(100%-1rem)] sm:w-[min(calc(100%-1.5rem),74rem)] pointer-events-none z-0">
-         <div className="absolute bottom-0 left-0 right-0 h-px bg-white/20" />
-         <div className="absolute bottom-[-2px] left-0 w-px h-[5px] bg-white/80" />
-         <div className="absolute bottom-[-2px] right-0 w-px h-[5px] bg-white/80" />
-         <div className="absolute bottom-0 left-[-2px] w-[5px] h-px bg-white/80" />
-         <div className="absolute bottom-0 right-[-2px] w-[5px] h-px bg-white/80" />
+        <div className="absolute bottom-0 left-0 right-0 h-px bg-white/35" />
+
+        {/* Bottom-left corner bracket */}
+        <div className="absolute bottom-0 left-0 w-[1.5px] h-[16px] bg-white/60" />
+        <div className="absolute bottom-0 left-0 w-[16px] h-[1.5px] bg-white/60" />
+
+        {/* Bottom-right corner bracket */}
+        <div className="absolute bottom-0 right-0 w-[1.5px] h-[16px] bg-white/60" />
+        <div className="absolute bottom-0 right-0 w-[16px] h-[1.5px] bg-white/60" />
       </div>
     </section>
   )
